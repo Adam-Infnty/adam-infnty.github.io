0a13a245d58702d140595eb57b24f349b26b9485
Adam <entei@MacBookPro.local>
2025-05-14 16:20:59 +0100

    alignment fix

--- script.js
+++ script.js
@@ -136,7 +136,7 @@ document.addEventListener("DOMContentLoaded", () => {
     sidebar.style.top = "0";
     sidebar.style.left = "0";
     sidebar.style.height = "100vh";
-    sidebar.style.width = "30rem";
+    sidebar.style.width = "27rem";
     sidebar.style.overflow = "auto";
   }
   
